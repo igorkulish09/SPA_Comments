00ce762fa5e841a48c6eff71ef31f3b76f3458a6
igor.kulish <igor.kulish09@gmail.com>
2023-12-05 21:07:15 +0200

    vue-commit

--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Comment(models.Model):
+    user_name = models.CharField(max_length=255)
+    email = models.EmailField()
+    captcha = models.CharField(max_length=10)
+    text = models.TextField()
+    parent_comment = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
--- comments/views.py
+++ comments/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Comment
+from .serializers import CommentSerializer
 
-# Create your views here.
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
